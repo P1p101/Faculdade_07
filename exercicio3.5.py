1a0522053d1be45a7a336570dec7e669be5a5bc7
P1p1__ <130181084+P1p101@users.noreply.github.com>
2023-05-04 16:15:54 -0300

    Add files via upload

---
+++ exercicio3.5.py
@@ -0,0 +1,12 @@
+# Calcular o resultado da expressão A > B and C or D, ultilizando os valores da tabela.
+a = 1
+b = 2
+print(a > b)
+
+C = True
+D = False
+print(C or D)
+
+ab = False
+cd = True
+print(ab or cd)
---
+++ exercico3.4.py
@@ -0,0 +1,7 @@
+# Escrever uma expressão para determinar se uma pessoa deve ou não pagar imposto.
+salario = 1400.00
+valor = 1200.00
+print(salario > valor)
+
+
+
